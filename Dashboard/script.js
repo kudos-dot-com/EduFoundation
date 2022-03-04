cd18ade699b35800430cff449f73579f56afb926
Arko27 <arkosen27@gmail.com>
2022-03-05 01:24:38 +0530

    dbupdate

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,23 +1,31 @@
 document.getElementById('jeem').addEventListener('click',() =>{
     var a = document.getElementById('jeem').textContent
     document.getElementById("dash").textContent=a
+
+    document.getElementById("sub3").textContent="Maths"
 })
 
 document.getElementById('jeea').addEventListener('click',() =>{
     
     var a = document.getElementById('jeea').textContent
     document.getElementById("dash").textContent=a
+
+    document.getElementById("sub3").textContent="Maths"
 })
 
 document.getElementById('neet').addEventListener('click',() =>{
     
     var a = document.getElementById('neet').textContent
     document.getElementById("dash").textContent=a
+    
+    document.getElementById("sub3").textContent="Biology"
 })
 
 document.getElementById('wbjee').addEventListener('click',() =>{
     
     var a = document.getElementById('wbjee').textContent
     document.getElementById("dash").textContent=a
+
+    document.getElementById("sub3").textContent="Maths"
 })
 
