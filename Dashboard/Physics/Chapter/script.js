ed91327f4c84942682243eaf343923c77508e68f
Arko27 <arkosen27@gmail.com>
2022-03-26 01:43:17 +0530

    api

---
+++ Dashboard/Physics/Chapter/script.js
@@ -0,0 +1,29 @@
+document.getElementById("pchsubmit").addEventListener("click",()=>{
+
+    var subject = window.location.pathname.split('/')[2].toLowerCase();
+    var name = document.getElementById("phytopic").value;
+
+    console.log(subject)
+    console.log(name)
+    if(subject == "")
+        alert("All Fields are Required")
+    else
+    {
+        document.getElementById("pchsubmit").textContent="Posting..."
+        fetch("http://localhost:8080/api/chapter/add",{
+            method: "POST",
+            body: JSON.stringify({
+                name,
+                subject
+                }),
+            headers:{
+                "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            .then(response => response.json())
+            .then(json => {
+                document.getElementById("pchsubmit").textContent="Submit"
+                console.log(json)
+                alert(json.message)});
+        }
+})
