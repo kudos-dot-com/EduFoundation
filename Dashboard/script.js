2b5f48f54944762d29c0c3d4f9c4af432f933eb1
Arko27 <arkosen27@gmail.com>
2022-03-26 23:14:44 +0530

    a

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -2,19 +2,16 @@ document.getElementById('jeem').addEventListener('click',() =>{
     
     var a = document.getElementById('jeem').textContent
     document.getElementById("dash").textContent=a
-    document.getElementById("mb").textContent = "Maths";
 })
 
 document.getElementById('neet').addEventListener('click',() =>{
     
     var a = document.getElementById('neet').textContent
     document.getElementById("dash").textContent=a
-    document.getElementById("mb").textContent = "Biology";
 })
 
 document.getElementById('wbjee').addEventListener('click',() =>{
     
     var a = document.getElementById('wbjee').textContent
     document.getElementById("dash").textContent=a
-    document.getElementById("mb").textContent = "Maths";
 })
