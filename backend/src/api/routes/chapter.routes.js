9d9d54788d12a267d743219499f2a63e866d70f1
bhaskar sengupta <sajal1943@gmail.com>
2022-04-03 12:50:22 +0530

    subjects added

--- backend/src/api/routes/chapter.routes.js
+++ backend/src/api/routes/chapter.routes.js
@@ -2,5 +2,6 @@ const {chapterController} = require('../controllers/chapter.controller')
 const router = require('express').Router();
 
 router.post('/add',chapterController.addChapter);
+router.get('/chapter/:subject/',chapterController.getChapter);
 
 module.exports = router;
