a5d2e33a2ffed8dbf563538a8967c163aeea9c7f
bhaskar sengupta <sajal1943@gmail.com>
2022-07-15 23:57:42 +0530

    chnages

--- backend/src/api/utils/uploadFile.js
+++ backend/src/api/utils/uploadFile.js
@@ -1,18 +1,24 @@
 var multer = require('multer');
-
+const { response, incompleteField } = require("../helpers/response");
 
   
      function initializeMulter(){
-        var storage = multer.diskStorage({ //multers disk storage settings
-            destination: function (req, file, cb) {
-                cb(null, 'src/api/public/')
-            },
-            filename: function (req, file, cb) {
-                var datetimestamp = Date.now();
-                cb(null,Date.now() + file.originalname)
-            }
-        });
-        var upload = multer({storage:storage})
-        return upload;
+        try{
+            var storage = multer.diskStorage({ //multers disk storage settings
+                destination: function (req, file, cb) {
+                    cb(null, 'src/api/public/')
+                },
+                filename: function (req, file, cb) {
+                    var datetimestamp = Date.now();
+                    cb(null,Date.now() + file.originalname)
+                }
+            });
+            var upload = multer({storage:storage})
+            return upload;
+        }
+        catch(err){
+      return response(res, "", "something went wrong while uploading ", 500);
+
+        }
 }
 module.exports = initializeMulter
